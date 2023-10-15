e5137ef82f2d878b42ddf7a8bd5de5968a95fee0
retrastore2 <146480009+retrastore2@users.noreply.github.com>
2023-10-15 14:06:40 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,19 +5,20 @@ Mʏ Nᴀᴍᴇ Is {bot}.
 
 I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ Oʀ Jᴏɪɴ Oᴜʀ Gʀᴏᴜᴘ</b>"""
     
-    HELP_TXT = "Hᴇʏ {}\nHᴇʀᴇ Mꜱ Mʏ Hᴇʟᴩ"
+    HELP_TXT = "Hᴇʏ {}\n𝚅𝙾𝙸𝙲𝙸 𝙻’𝙰𝙸𝙳𝙴 𝙿𝙾𝚄𝚁 𝙼𝙴𝚂 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝙴𝚂."
 
-    ABOUT_TXT = """<b>Mʏ ɴᴀᴍᴇ: {}
-✯ Dᴇᴠᴇʟᴏᴩᴇʀ: <a herf=https://t.me/cinemalakay_group>✪ zoѕ-мan ✪</a>
-✯ Cᴏᴅᴇᴅ Oɴ: ᴩʏᴛʜᴏɴ/ᴩʏʀᴏɢʀᴀᴍ
-✯ Mʏ DᴀᴛᴀBᴀꜱᴇ: ᴍᴏɴɢᴏ-ᴅʙ
-✯ Mʏ Sᴇʀᴠᴇʀ: ᴀɴʏᴡʜᴇʀᴇ
-✯ Mʏ Vᴇʀꜱɪᴏɴ: ᴩʀᴏꜰᴇꜱꜱᴏʀ-ʙᴏᴛ ᴠ4.5.0</b>"""
-   
+ABOUT_TXT = """<b>✯ 𝙼𝙾𝙽 𝙽𝙾𝙼: {𝐏𝐑𝐎𝐅𝐄𝐒𝐎𝐑}
+✯ 𝙲𝚁𝙴𝙰𝚃𝙴𝚄𝚁: <a herf=https://t.me/zosman_official>✪ zoѕ-мan ✪</a>
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: ᴍᴏɴɢᴏ-ᴅʙ
+✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: ᴩʀᴏꜰᴇꜱꜱᴏʀ-ʙᴏᴛ ᴠ4.5.0 [ BETA ]</b>"""
+
     SOURCE_TXT = """<b>NOTE:</b>
-- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href=https://github.com/MrMKN/PROFESSOR-BOT>𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑-𝐁𝐎𝐓</a>
+- ꜱᴏᴜʀᴄᴇ ◉› :<a href=https://t.me/Hyman_Official>𝐇𝐘𝐌𝐀𝐍</a>
 
-<b>ᴅᴇᴠ: <a herf=https://t.me/cinemalakay_group>✪ zoѕ-мan ✪</a></b>"""
+<b>ᴅᴇᴠ: <a herf=https://t.me/zosman_official>✪ zoѕ-мan ✪</a></b>"""
 
     FILE_TXT = """<b>➤ Hᴇʟᴘ Fᴏʀ Fɪʟᴇ Sᴛᴏʀᴇ</b>
 
